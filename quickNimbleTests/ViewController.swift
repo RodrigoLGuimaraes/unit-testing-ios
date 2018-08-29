d7bbad79f620c88ade3a4520ab5f71c78d43fdd5
RodrigoLGuimaraes <rolongui@yahoo.com.br>
2018-08-29 11:59:51 -0300

    Initial Commit

---
+++ quickNimbleTests/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  quickNimbleTests
+//
+//  Created by Rodrigo Longhi Guimarães on 29/08/18.
+//  Copyright © 2018 Rodrigo Longhi Guimarães. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
